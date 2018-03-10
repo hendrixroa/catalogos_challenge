848a709e6d61f624a898f02c85a391b3d14ac5a4
Hendrix Roa <hendrix.alex87@gmail.com>
2018-03-10 10:12:23 -0500

    metodo poss customizando para agregar detalle

--- catalogos_challenge/settings.py
+++ catalogos_challenge/settings.py
@@ -33,7 +33,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -107,8 +106,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+CSRF_COOKIE_SECURE = True
 
 STATIC_URL = '/static/'
--- productos/views.py
+++ productos/views.py
@@ -1,7 +1,10 @@
-from productos.models import Product, ProductDetail
 from django.http import HttpResponse
 from django.core import serializers
+from django.core.exceptions import ValidationError
+import json
 from django.views import View
+from productos.models import Product, ProductDetail
+
 class ProductsList(View):
     model = Product
 
@@ -10,4 +13,19 @@ class ProductsList(View):
         print(products)
         serializer = serializers.serialize('json', products)
         return HttpResponse(serializer, content_type='application/json')
-    
+
+    def post(self, request, *args, **kwargs): 
+        try:   
+            data_body = json.loads(request.body.decode('utf8'))
+            detail = {}
+            for item in data_body.items():
+                detail.update({ 'price_offer': item.pop('price_offer') })
+            print(data_body.items())
+            product = Product(**data_body)
+            product.full_clean()
+            Product.objects.create(**data_body)
+            return HttpResponse(json.dumps({'status': 200, 'message': 'Producto Creado'}), content_type='application/json')
+        except ValidationError as e:
+            response = HttpResponse(json.dumps(e.message_dict), content_type='application/json') 
+            response.status_code = 400
+            return response
